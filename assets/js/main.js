151aeacaf5c33842903ecd88b0a09ef4b3626ea9
Devendra Awasthi <devendrasharma1211@gmail.com>
2021-03-09 23:25:11 +0530

    new design launch

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+
+!(function($) {
+  "use strict";
+
+  // Back to top button
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+
+  $('.back-to-top').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+  });
+ //AOS Animation
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      once: true
+    });
+  }
+  $(window).on('load', function() {
+    aos_init();
+  });
+})(jQuery);
